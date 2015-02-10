df24965a9b3b05c3a4ad012d665927aeaf9480bc
coojee2012 <11366846@qq.com>
2015-02-10 16:43:27 +0800

    提交一致性

---
+++ MyInit/config.php
@@ -0,0 +1,35 @@
+<?php
+// database host
+$db_host   = "localhost:3306";
+
+// database name
+$db_name   = "eshop";
+
+// database username
+$db_user   = "root";
+
+// database password
+$db_pass   = "12345678";
+
+// table prefix
+$prefix    = "es_";
+
+$timezone    = "UTC";
+
+$cookie_path    = "/";
+
+$cookie_domain    = "";
+
+$session = "1440";
+
+define('EC_CHARSET','utf-8');
+
+define('ADMIN_PATH','admin');
+
+define('AUTH_KEY', 'this is a key');
+
+define('OLD_AUTH_KEY', '');
+
+define('API_TIME', '2014-12-15 06:00:52');
+
+?>
